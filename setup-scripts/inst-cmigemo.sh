ef4544e8ab3072f584fc2425a74c103fd6be2fec
deresmos <deresmos@gmail.com>
2017-06-25 23:10:10 +0900

    Update cmigemo install file

--- setup-scripts/inst-cmigemo.sh
+++ setup-scripts/inst-cmigemo.sh
@@ -6,24 +6,24 @@ fi
 
 cd cmigemo
 
-if [ "$(uname)" == 'Darwin' ]; then
-	if ! hash nkf 2>/dev/null; then
-		if ! hash brew 2>/dev/null; then
-			echo 'You must install brew'
-			exit 1
-		fi
-		brew install nkf
-	fi
+if ! hash nkf 2>/dev/null; then
+  if [ "$(uname)" == 'Darwin' ]; then
+    if ! hash brew 2>/dev/null; then
+      echo 'You must install brew'
+      exit 1
+    fi
+    brew install nkf
 
-	./configure &&
-	make osx &&
-	make osx-dict &&
-	sudo make osx-install
-else
-	./configure &&
-	make gcc &&
-	make gcc-dict &&
-	sudo make gcc-install
+    ./configure &&
+    make osx &&
+    make osx-dict &&
+    sudo make osx-install
+  else
+    ./configure &&
+    make gcc &&
+    make gcc-dict &&
+    sudo make gcc-install
+  fi
 fi
 
 echo 'Installed cmigemo'
