7d206cee0713659341bcefe1d4c8baacf7f83cdd
deresmos <deresmos@gmail.com>
2017-09-04 21:24:44 +0900

    Refactoring

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -2,25 +2,19 @@
 
 set -x
 dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-nvim_dir=$HOME'/.config/nvim'
-nvim_dein_dir=$nvim_dir'/dein'
+nvim_dir=$HOME/.config/nvim
+nvim_dein_dir=$nvim_dir/dein
 
 # vim/deinディレクトリ作成
-if [ ! -d $nvim_dein_dir ]; then
-	mkdir -p $nvim_dein_dir
-fi
+[ -d $nvim_dein_dir ] || mkdir -p $nvim_dein_dir
 
 # オリジナルのvimrcを退避
-if [ -f $HOME/.vimrc ]; then
-	if [ ! -f $HOME/.vimrc.ORIG ]; then
-		cp $HOME/.vimrc $HOME/.vimrc.ORIG
-	fi
-fi
+[ -f $HOME/.vimrc ] && ([ -f $HOME/.vimrc.ORIG ] || cp $HOME/.vimrc $HOME/.vimrc.ORIG)
 
 # シンボリックリンク貼り貼り
 ln -sf ${dir}/basic.vim $HOME/
 ln -sf ${dir}/.ideavimrc $HOME/
-ln -sf ${dir}/init.vim $nvim_dir'/'
-ln -sf ${dir}/my.vim $nvim_dir'/my.vim'
-ln -sf ${dir}/dein/dein.toml $nvim_dein_dir'/'
-ln -sf ${dir}/dein/dein_lazy.toml $nvim_dein_dir'/'
+ln -sf ${dir}/init.vim $nvim_dir/
+ln -sf ${dir}/my.vim $nvim_dir/
+ln -sf ${dir}/dein/dein.toml $nvim_dein_dir/
+ln -sf ${dir}/dein/dein_lazy.toml $nvim_dein_dir/
