cd89e9695d17867acf4783b25391c263c6645faf
deresmos <deresmos@gmail.com>
2018-03-20 00:09:34 +0900

    Add CLP

--- setup-scripts/inst-npm-package.sh
+++ setup-scripts/inst-npm-package.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-npm install -g csslint jshint ternjs eslint stylelint csscomb
+npm install -g csslint jshint ternjs eslint stylelint csscomb vue-language-server javascript-typescript-langserver
--- setup-scripts/inst-pip-package.sh
+++ setup-scripts/inst-pip-package.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-pip install yapf flake8 isort autopep8 neovim-remote jedi vim-vint proselint yamllint
+pip install yapf flake8 isort autopep8 neovim-remote jedi vim-vint proselint yamllint python-language-server
